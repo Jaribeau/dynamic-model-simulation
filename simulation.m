0e341c834b17523f20c03b7ec38710e8f4f461dc
JaredBaribeau <jared.baribeau@gmail.com>
2016-07-25 15:00:55 -0400

    Post TA meeting with fixed state equations

--- simulation.m
+++ simulation.m
@@ -3,25 +3,22 @@
 
 close all
 global k1 k3 r j h m g b;
-%% Set Parameters and Constants
-k1 = 3;     % Spring 1 coefficient
-k3 = 3;     % Spring 2 coefficient
-r = 0.0381; % Radius of mass 3.81cm (m)
-j = 0.7;    % Moment of innertia of wheel (936g)
-h = 0.1;    % L = length of chain above the table at rest (m)
-m = 0.025;  % m = mass of chain above the table at rest (kg)
-b = 0;      % Coeficcient of friction in the rotational shaft
-g = 9.81;   
-
+%% Set Parameters and Constaznts
+k1 = 0.4232;    % Spring 1 coefficient
+k3 = 0.4232;    % Spring 2 coefficient
+r = 0.0381;     % Radius of mass 3.81cm (m)
+j = 0.0002;     % Moment of innertia of wheel (936g)
+h = 0.2394;       % L = length of chain above the table at rest (m)
+m = 0.035;      % m = mass of chain above the table at rest (kg)
+b = 0.0005;     % Coeficcient of friction in the rotational shaft
+g = 9.81;       % Gravity 
 
 %% Set Initial Conditions
 
-q1 = 0;   % Potential energy in spring 1 (TODO: update this spring equation)
-p2 = 0;     % Angular momentum of pulley (zero when at rest) 
-q3 = 0.3;   % Potential energy in spring 2 (TODO: update this spring equation) 
-p4 = 0;     % Momentum of chain (zero when at rest)
-% m4 = chainMass( dL, m, L ) -> see function in chainMass.m 
-
+q1 = 0.13;      % Potential energy in spring 1 (TODO: update this spring equation)
+p2 = 0;         % Angular momentum of pulley (zero when at rest) 
+q3 = 0;         % Potential energy in spring 2 (TODO: update this spring equation) 
+p4 = 0;         % Momentum of chain (zero when at rest)
 
 %% Run Simulation:
 
@@ -34,22 +31,30 @@ y0 = [q1; p2; q3; p4];
 p = [k1; k3; r; j; m; h];
 
 [T, y] = ode45(@OurCoolSystem, tspan, y0);
-y
-% y(:,3)
+
 %% Plot Results
-subplot(2,2,1)
+subplot(3,3,1)
 plot(T, y)  % Plot everything
 legend('q1 - PE Spring 1', 'p2 - Ang. momentum', 'q3 - PE Spring 2', 'p4 - Mass Momentum')
 
-subplot(2,2,2)
+subplot(3,3,2)
 plot(T, y(:,1))  % Plot spring 1
 legend('q1 - PE Spring 1')
 
-subplot(2,2,4)
+subplot(3,3,5)
 plot(T, y(:,3))  % Plot spring 2
 legend('q3 - PE Spring 2')
 
-subplot(2,2,3)
-plot(T, (y(:,1) + y(:,3)))
-legend('q1 + q3')
+subplot(3,3,6)
+plot(T, (y(:,4)))
+legend('p4')
+
+subplot(3,3,4)
+plot(T, (0.2394 - (y(:,3) - y(:,1))))
+legend('height')
+
+subplot(3,3,3)
+plot(T, (y(:,2)))
+legend('p2')
+
 
